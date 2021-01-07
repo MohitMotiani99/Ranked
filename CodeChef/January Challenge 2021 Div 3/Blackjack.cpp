973bde5b574c00f612e1d2b50ebf3de1ca37097c
Mohit Motiani <34206001+MohitMotiani99@users.noreply.github.com>
2021-01-08 00:21:18 +0530

    Create Blackjack.cpp

---
+++ CodeChef/January Challenge 2021 Div 3/Blackjack.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+int helper(int arr[],int n,int swaps,int x,int y,int ind,long sum){
+    if(ind>=n){
+        cout<<"H1"<<endl;
+        if(sum<x)
+        return INT_MAX;
+    }
+    if(sum>=x && sum<=y)
+    {
+        cout<<"H2"<<" "<<swaps<<endl;
+        return swaps;
+    }
+    
+    int res=INT_MAX;
+    
+        
+        //int f=1;
+        for(int i=ind;i<n;i++){
+            if(!(sum+arr[i]>y))
+            {
+                //f=0;
+                swap(arr[ind],arr[i]);
+                int ans=helper(arr,n,swaps+1,x,y,ind+1,sum+arr[ind]);
+                cout<<"Sum: "<<sum<<" "<<"Swaps: "<<swaps<<"  ";
+                swap(arr[ind],arr[i]);
+                cout<<"Ans: "<<ans<<"  ";
+                res=min(res,ans);
+            }
+        }
+        cout<<"H3"<<" "<<res<<endl;
+        return res;
+    
+    
+    //cout<<"H4"<<" "<<swaps<<endl;
+    //return helper(arr,n,swaps,x,y,ind+1,sum+arr[ind]);
+    //return 0;
+}
+int main(){
+    int t,n,x,y;
+    cin>>t;
+    while(t--){
+        cin>>n>>x>>y;
+        int arr[n];
+        for(int i=0;i<n;i++)cin>>arr[i];
+        
+        int swaps=0;
+        long sum=0;
+        cout<<helper(arr,n,swaps,x,y,0,sum)<<endl;
+    }
+}
