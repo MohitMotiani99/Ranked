9865e1f8637a874e2a7e384cd211c7106bca1b51
Mohit Motiani <34206001+MohitMotiani99@users.noreply.github.com>
2021-01-13 17:36:20 +0530

    Update BOX REDUCTION.cpp

--- CodeChef/pop20push21/BOX REDUCTION.cpp
+++ CodeChef/pop20push21/BOX REDUCTION.cpp
@@ -6,26 +6,114 @@ int main(){
     int arr[n];
     for(int i=0;i<n;i++)cin>>arr[i];
     
-    sort(arr,arr+n);
-    if(n==1)
-    cout<<"1"<<endl;
-    else{
-        //map<int,int> m;
-        //bool vis[n];
-        //for(int i=0;i<n;i++)
-        //vis[n]=false;
-        int c=0;
-        for(int i=n-1,j=n-2;j>=0;){
-            if(arr[i]>=(2*arr[j])){
-                //m[i]=j;
-                //vis[j]=true;
-                c++;
-                i=j-1;
-                j--;
-            }
-            else
-                j--;
+    sort(arr,arr+n,greater<int>()) ;
+    
+    int m=0,d;
+    if(n%2==0)
+    d=n/2;
+    else
+    d=(n+1)/2;
+    
+    int c=0;
+    for(int i=d;i<n;i++){
+        if(2*arr[i]<=arr[m]){
+            c++;
+            m++;
         }
-        cout<<n-c<<endl;
     }
+    
+    cout<<n-c<<endl;
 }
+/*
+16 1 4 8 2
+16 8 4 2 1
+
+d=3
+m=0
+c=0
+
+
+i=3
+    2*2<=16===>T
+        c=1
+        m=1
+
+i=4
+    2*1<=8===>T
+        c=2
+        m=2
+
+print 5-2=3
+
+
+
+
+
+
+
+16 10 8 4 2 1
+
+d=3
+m=0
+c=0
+
+
+i=3
+    T
+    c=1
+    m=1
+
+i=4
+    T
+    c=2
+    m=2
+
+i=5
+    T
+    c=3
+    m=3
+    
+print 3
+
+
+
+
+
+
+
+16 10 8 4 2 1
+
+c=0
+
+i=0
+j=1
+
+    F
+    c=0
+    j=2
+
+i=0
+j=2
+
+    T
+    c=1
+    i=1
+    j=3
+    
+i=1
+j=3
+
+    T
+    c=2
+    i=4
+    j=5
+    
+i=4
+j=5
+
+    T
+    c=3
+    i=6
+    j=7
+
+*/
