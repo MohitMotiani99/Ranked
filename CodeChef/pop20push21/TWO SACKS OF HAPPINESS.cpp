5771c59e39b20cfba2575bb367351a2b5204004f
Mohit Motiani <34206001+MohitMotiani99@users.noreply.github.com>
2021-01-15 23:30:23 +0530

    Can use kadane for max cont sum at each index,both fwd and backwd

--- CodeChef/pop20push21/TWO SACKS OF HAPPINESS.cpp
+++ CodeChef/pop20push21/TWO SACKS OF HAPPINESS.cpp
@@ -1,58 +1,53 @@
 #include<bits/stdc++.h>
+
 using namespace std;
-bool cmp(pair<pair<int,int>,long> a ,pair<pair<int,int>,long> b){
-    return a.second<b.second;
-}
+
 int main(){
-    int n;
+    long n;
+    
     cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++)cin>>arr[i];
+    long arr[n];
     
-    map<pair<int,int>,long> m;
-    long sum=0;
-    int beg=-1;
-    for(int i=0;i<n;i++){
-        if(arr[i]>0 && beg==-1){
-        sum+=arr[i];
-        beg=i;
-        }
-        else if(arr[i]>0 && beg>=0)
-        sum+=arr[i];
-        else if(arr[i]<=0 && sum>0){
-            m.insert({{beg,i-1},sum});
-            sum=0;
-            beg=-1;
-        }
-    }
-    if(sum>0){
-        m.insert({{beg,n-1},sum});
-        sum=0;
-        beg=-1;
+    for(long i=0;i<n;i++)cin>>arr[i];
+    
+    
+    
+    long lp[n],rp[n];
+    long msf=INT_MIN,meh=0;
+    for(long i=0;i<n;i++){
+        meh=meh+arr[i];
+        if(msf<meh)
+        msf=meh;
+        
+        if(meh<0)
+        meh=0;
+        
+        lp[i]=msf;
     }
     
-    for(auto i=m.begin();i!=m.end();i++){
-        cout<<(i->first).first<<" "<<(i->first).second<<"    "<<i->second<<endl;
+    //for(int i=0;i<n;i++)
+    //cout<<lp[i]<<" ";
+    //cout<<endl<<endl;
+    
+    msf=INT_MIN;meh=0;
+    for(long i=n-1;i>=0;i--){
+        meh=meh+arr[i];
+        if(msf<meh)
+        msf=meh;
+        
+        if(meh<0)
+        meh=0;
+        
+        rp[i]=msf;
     }
     
-    if(m.size()==0){
-        sort(arr,arr+n);
-        cout<<arr[n-1]+arr[n-2]<<endl;
+    //for(int i=0;i<n;i++)
+    //cout<<rp[i]<<" ";
+    //cout<<endl<<endl;
+    
+    long ans=INT_MIN;
+    for(int i=0;i<n-1;i++){
+        ans=max(ans,lp[i]+rp[i+1]);
     }
-    else if(m.size()==1){
-        if((m.begin()->first).first==(m.begin()->first).second){
-            sort(arr,arr+n);
-            cout<<arr[n-1]+arr[n-2]<<endl;
-        }
-        else
-        cout<<(m.begin()->second)<<endl;
-    }
-    else{
-        vector<pair<pair<int,int>,long> > v;
-        for(auto &i : m){
-            v.push_back(i);
-        }
-        sort(v.begin(),v.end(),cmp);
-        cout<<v[v.size()-1].second+v[v.size()-2].second<<endl;
-    }
+    cout<<ans<<endl;
 }
