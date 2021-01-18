2f3eba1d951dc19285ada55a633dfb474218a605
Mohit Motiani <34206001+MohitMotiani99@users.noreply.github.com>
2021-01-18 18:23:50 +0530

    Create F-K Wrong.cpp

---
+++ CodeChef/push_back(4)/F-K Wrong.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(0);
+   cin.tie(NULL);
+   cout.tie(NULL);
+   
+   int t,n;
+   cin>>t;
+   while(t--){
+       scanf("%d",&n);
+       //cout<<n*(n+1)<<endl;
+       unsigned long long a=n*(n+1);
+       printf("%llu \n",a);
+       
+   }
+}
