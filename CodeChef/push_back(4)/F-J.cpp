ffda82a2a019217c5d1597f87bd78fd029b3d879
Mohit Motiani <34206001+MohitMotiani99@users.noreply.github.com>
2021-01-18 17:56:26 +0530

    Create F-J.cpp

---
+++ CodeChef/push_back(4)/F-J.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t,n;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        int s=n/2-1;
+        int col=s+n/2;
+        int r=n;
+        int c=1;
+        //cout<<r<<" "<<col<<endl;
+        int space =n/2;
+        int dp[r][c];
+        for(int i=0;i<r;i++){
+            if(i==n/2 + 1)
+            space=1;
+            int k=0;
+            for(int j=0;j<col;j++){
+                if(j<space)
+                cout<<" ";
+                else if(k==s)
+                cout<<" ";
+                else
+                {cout<<c;c++;k++;}
+                
+                //cout<<dp[i][j]<<" ";
+            }
+            if(i<=n/2)
+            space--;
+            else
+            space++;
+            cout<<endl;
+        }
+        
+        
+        
+    }
+}
