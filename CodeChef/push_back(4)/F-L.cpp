819afeb9f83f515da1200bfd15d100faf9203f79
Mohit Motiani <34206001+MohitMotiani99@users.noreply.github.com>
2021-01-27 10:30:24 +0530

    use printf,scanf for fast io

---
+++ CodeChef/push_back(4)/F-L.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //ios_base::sync_with_stdio(0);
+   //cin.tie(NULL);
+   
+   long long t,n;
+   scanf("%lld",&t);
+   while(t--){
+       scanf("%lld",&n);
+       printf("%lld \n", n*n);
+       
+   }
+}
