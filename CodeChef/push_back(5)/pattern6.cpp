97910b46dd22b698a44da6addf0e19d7bebb14ca
Mohit Motiani <34206001+MohitMotiani99@users.noreply.github.com>
2021-02-03 08:16:25 +0530

    Create pattern6.cpp

---
+++ CodeChef/push_back(5)/pattern6.cpp
@@ -0,0 +1,195 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve(int n){
+    int c=1;
+    for(int i=1;i<=n;i++){
+        if(i%2==1){
+            //cout<<c<<endl;
+            for(int j=1;j<=2*i-1;j++){
+                if(j%2==1)
+                {cout<<c;c++;}
+                else
+                cout<<"*";
+                
+            }
+        }
+        else{
+            //cout<<c<<endl;
+            int newr=c+i-1;
+            int nes=newr;
+            for(int j=1;j<=2*i-1;j++){
+                if(j%2==1)
+                {cout<<newr;newr--;}
+                else
+                cout<<"*";
+                
+                
+            }
+            c=nes+1;
+        }
+        cout<<endl;
+    }
+}
+void solve2(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-1;j++)
+        {
+            if(j==i)
+            cout<<"*";
+            else
+            cout<<"0";
+        }
+        cout<<"*";
+        for(int j=1;j<=n-1;j++)
+        {
+            if(n-j==i)
+            cout<<"*";
+            else
+            cout<<"0";
+        }
+        
+        // for(int j=0;j<n-i;j++)
+        // cout<<"*";
+        
+        // for(int j=n-i;j>=1;j--)
+        // cout<<j;
+        
+        
+        
+        cout<<endl;
+    }
+}
+void solve4(int n){
+    char ch;
+        if(n%2==1)
+        ch='*';
+        else
+        ch='A';
+        
+    for(int i=1;i<=n;i++){
+        for(int space=1;space<=n-i;space++)
+        cout<<" ";
+        
+        
+        for(int j=0;j<2*i-1;j++){
+            cout<<ch;
+            if(ch=='*')
+            ch='A';
+            else
+            ch='*';
+        }
+        
+        cout<<endl;
+    }
+}
+void solve3(int n){
+    vector<vector<char> > v(n);
+    for(int i=1;i<=n/2+1;i++){
+        v[i-1].push_back(i+48);
+        
+        for(int j=0;j<i-1;j++)
+        v[i-1].push_back('*'),v[i-1].push_back(i+48);
+    }
+    
+    int c=2;
+    for(int i=n/2-1;i>=0;i--,c+=2){
+        v[i+c]=v[i];
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<v[i].size();j++)
+        cout<<v[i][j];
+        
+        cout<<endl;
+    }
+}
+void solve5(int n){
+    for(int i=1;i<=n;i++){
+        int gap=n-1;
+        int c=0;
+        int start=i;
+        while(c<i){
+            cout<<start;
+            start+=gap;
+            gap--;
+            c++;
+        }
+        cout<<"*";
+        cout<<endl;
+    }
+}
+void solve6(int n){
+    int i;
+    for(i=1;i<=n/2+1;i++){
+        
+            if(i%2==1){
+                for(int j=1;j<=i;j++)
+                cout<<"*";
+                
+                for(int j=1;j<=n-i;j++)
+                cout<<"#";
+            }
+            else{
+                for(int j=1;j<=n-i;j++)
+                cout<<"#";
+                
+                for(int j=1;j<=i;j++)
+                cout<<"*";
+            }
+            
+            cout<<endl;
+    }
+
+    if(i%2==0){
+        i--;
+    for(;i<n;i++){
+        
+            if(i%2==1){
+                for(int j=1;j<=i;j++)
+                cout<<"#";
+                
+                for(int j=1;j<=n-i;j++)
+                cout<<"*";
+            }
+            else{
+                for(int j=1;j<=n-i;j++)
+                cout<<"*";
+                
+                for(int j=1;j<=i;j++)
+                cout<<"#";
+            }
+            
+            cout<<endl;
+    }
+    }
+    else{
+        i-=2;
+        for(;i<n-1;i++){
+        
+            if(i%2==1){
+                for(int j=1;j<=i;j++)
+                cout<<"*";
+                
+                for(int j=1;j<=n-i;j++)
+                cout<<"#";
+            }
+            else{
+                for(int j=1;j<=i;j++)
+                cout<<"#";
+                
+                for(int j=1;j<=n-i;j++)
+                cout<<"*";
+            }
+            
+            cout<<endl;
+    }
+    }
+    
+}
+int main(){
+    int t,n;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        solve6(n);
+    }
+}
